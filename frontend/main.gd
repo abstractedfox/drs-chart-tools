97dc4888dfca4a4dcab2ca8d247453ab63cdb0fb
Chris <chriswhoprograms@gmail.com>
2024-08-22 14:32:53 -0400

    add godot project

---
+++ frontend/main.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+
+func apiTest():
+	var command = "python3.12 chart_tools_api.py init testyy.xml : bpm add 57300 0"
+	var cmdReturn = []
+	
+	OS.execute("which", ["python3.12"], cmdReturn)
+	#OS.execute("python3.12", ["chart_tools_api.py init godottestyy.xml : bpm add 57300 0"], cmdReturn, true)
+	
+	print_debug("we here")
+	print_debug("ayo? " + cmdReturn[0])
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	apiTest()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass

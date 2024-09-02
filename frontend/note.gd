007afbcfdadd7143bace6b6445d68093fa259a90
Chris <chriswhoprograms@gmail.com>
2024-09-01 21:41:24 -0400

    added some boilerplate to Hopefully turn into dynamic note generation

--- frontend/note.gd
+++ frontend/note.gd
@@ -1,5 +1,10 @@
 extends Node3D
 
+var tmpMesh = Mesh.new()
+var vertices = PackedVector3Array()
+var UVs = PackedVector2Array()
+var mat = StandardMaterial3D.new()
+var color = Color(0, 0, 10)
 enum kind {LEFT, RIGHT}
 
 @export
@@ -18,8 +23,42 @@ var mesh
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	mesh = get_node("MeshInstance3D")
+	#mesh = get_node("MeshInstance3D")
+	vertices.push_back(Vector3(100,0,0))
+	vertices.push_back(Vector3(100,0,100))
+	vertices.push_back(Vector3(0,0,100))
+	vertices.push_back(Vector3(0,0,0))
 	
+	'''
+	vertices.push_back(Vector3(100,100,0))
+	vertices.push_back(Vector3(100,100,100))
+	vertices.push_back(Vector3(0,100,100))
+	vertices.push_back(Vector3(0,100,0))
+	
+	UVs.push_back(Vector2(0,0))
+	UVs.push_back(Vector2(0,1))
+	UVs.push_back(Vector2(1,1))
+	UVs.push_back(Vector2(1,0))
+	'''
+	UVs.push_back(Vector2(0,0))
+	UVs.push_back(Vector2(0,1))
+	UVs.push_back(Vector2(1,1))
+	UVs.push_back(Vector2(1,0))
+	
+	mat.albedo_color = color
+	
+	var surfacetool = SurfaceTool.new()
+	surfacetool.begin(Mesh.PRIMITIVE_TRIANGLES)
+	surfacetool.set_material(mat)
+	
+	for vertex in vertices.size():
+		surfacetool.set_color(color)
+		surfacetool.set_uv(UVs[vertex])
+		surfacetool.add_vertex(vertices[vertex])
+		
+	surfacetool.commit(tmpMesh)
+	
+	$MeshInstance3D.mesh = tmpMesh
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
